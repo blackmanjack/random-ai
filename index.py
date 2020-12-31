3abe1d1a7f09c879e5514cd6f4d243235d645b87
Nurul Akbar al-Ghifari <46098272+nurulakbaral@users.noreply.github.com>
2020-12-31 09:08:15 +0700

    final

--- index.py
+++ index.py
@@ -2,6 +2,7 @@
 
 # // Import library
 import csv
+from collections import deque
 
 # // Get data
 CsvPath = '/home/nufik/Documents/Kuliah/Tugas/AI/data.csv'
@@ -33,63 +34,47 @@ class TreeNode(object):
 class AVL_Tree(object):
 
     def insert(self, root, key):
-
         if not root:
             return TreeNode(key)
         elif key < root.val:
             root.left = self.insert(root.left, key)
         else:
             root.right = self.insert(root.right, key)
-
         root.height = 1 + max(self.getHeight(root.left),
                               self.getHeight(root.right))
-
         balance = self.getBalance(root)
-
         if balance > 1 and key < root.left.val:
             return self.rightRotate(root)
-
         if balance < -1 and key > root.right.val:
             return self.leftRotate(root)
-
         if balance > 1 and key > root.left.val:
             root.left = self.leftRotate(root.left)
             return self.rightRotate(root)
-
         if balance < -1 and key < root.right.val:
             root.right = self.rightRotate(root.right)
             return self.leftRotate(root)
-
         return root
 
     def leftRotate(self, z):
-
         y = z.right
         T2 = y.left
-
         y.left = z
         z.right = T2
-
         z.height = 1 + max(self.getHeight(z.left),
                            self.getHeight(z.right))
         y.height = 1 + max(self.getHeight(y.left),
                            self.getHeight(y.right))
-
         return y
 
     def rightRotate(self, z):
-
         y = z.left
         T3 = y.right
-
         y.right = z
         z.left = T3
-
         z.height = 1 + max(self.getHeight(z.left),
                            self.getHeight(z.right))
         y.height = 1 + max(self.getHeight(y.left),
                            self.getHeight(y.right))
-
         return y
 
     def getHeight(self, root):
@@ -112,12 +97,26 @@ class AVL_Tree(object):
         self.depthFirstSearch(root.left, target)
         self.depthFirstSearch(root.right, target)
 
+    def bestFirstSearch(self, root, target):
+        queue = deque()
+        queue.append(root)
+        while queue:
+            global isDocumentExist
+            curr = queue.popleft()
+            # print(curr.val, end=' ')
+            if curr.val == target:
+                isDocumentExist = True
+            if curr.left:
+                queue.append(curr.left)
+            if curr.right:
+                queue.append(curr.right)
+
 # // Init the Tree
 myTree = AVL_Tree()
 root = None
 
 # // Properties!
-target = 439785960
+target = 739322206
 isDocumentExist = False
 
 # // Create the AVL Tree! (Insertion)
@@ -127,7 +126,8 @@ for index, document in enumerate(documents):
         root = myTree.insert(root, isbn)
 
 # // Search a document with Dept First Search
-myTree.depthFirstSearch(root, target)
+# // myTree.depthFirstSearch(root, target)
+myTree.bestFirstSearch(root, target)
 
 # // The result
 if isDocumentExist == True:
